c6545f1350492068646660fc0da270db68b111d9
Alexey Krylov <alexey2142@gmail.com>
2013-12-04 06:00:44 +1100

    Добавил календарь на страницу изменения проекта

--- app/views/admin/projects/edit.blade.php
+++ app/views/admin/projects/edit.blade.php
@@ -13,12 +13,12 @@
 
         <li>
             {{ Form::label('date_start', 'Дата начала:') }}
-            {{ Form::text('date_start') }}
+            {{ Form::text('date_start', $project->date_start) }}
         </li>
 
         <li>
             {{ Form::label('date_end', 'Дата окончания:') }}
-            {{ Form::text('date_end') }}
+            {{ Form::text('date_end', $project->date_end) }}
         </li>
 
 		<li>
@@ -33,5 +33,14 @@
 		{{ implode('', $errors->all('<li class="error">:message</li>')) }}
 	</ul>
 @endif
+
+<link rel="stylesheet" type="text/css" href="/plugins/anytime/anytime.compressed.css" />
+<script src="/plugins/anytime/jquery-migrate-1.0.0.js"></script><!-- if jQuery 1.9+ -->
+<script src="/plugins/anytime/anytime.compressed.js"></script>
+
+<script>
+$('#date_start').AnyTime_noPicker().AnyTime_picker().focus();
+$('#date_end').AnyTime_noPicker().AnyTime_picker().focus();
+</script>
 
 @stop
