3c521d298084e3dead6458e1c721e34964bd7aa6
Alexey Krylov <alexey2142@gmail.com>
2013-12-04 12:45:19 +1100

    Добавил сиды для проектов

--- app/database/seeds/ProjectsTableSeeder.php
+++ app/database/seeds/ProjectsTableSeeder.php
@@ -5,14 +5,16 @@ class ProjectsTableSeeder extends Seeder {
 	public function run()
 	{
 		// Uncomment the below to wipe the table clean before populating
-		// DB::table('projects')->truncate();
+		DB::table('projects')->truncate();
 
-		$projects = array(
-
+		$projects[] = array(
+			'name' => 'Awesome project',
+			'date_start' => '2013-12-04 12:00',
+			'date_end' => '2013-12-15 12:00',
 		);
 
 		// Uncomment the below to run the seeder
-		// DB::table('projects')->insert($projects);
+		DB::table('projects')->insert($projects);
 	}
 
 }
