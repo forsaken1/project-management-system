8e26db9dde86dec3b8bdce6c5189c2e0bcbfa47b
Alexey Krylov <alexey2142@gmail.com>
2013-12-04 12:47:28 +1100

    Миграции и сиды: employee

---
+++ app/database/seeds/EmployeeTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+class EmployeeTableSeeder extends Seeder {
+
+	public function run()
+	{
+		// Uncomment the below to wipe the table clean before populating
+		DB::table('employee')->truncate();
+
+		$employee[] = array(
+			'user_id' => '2',
+			'project_id' => '1',
+			'is_manager' => '1',
+		);
+
+		$employee[] = array(
+			'user_id' => '3',
+			'project_id' => '1',
+			'is_manager' => '0',
+		);
+
+		$employee[] = array(
+			'user_id' => '4',
+			'project_id' => '1',
+			'is_manager' => '0',
+		);
+
+		$employee[] = array(
+			'user_id' => '5',
+			'project_id' => '1',
+			'is_manager' => '0',
+		);
+
+		// Uncomment the below to run the seeder
+		DB::table('employee')->insert($employee);
+	}
+
+}
