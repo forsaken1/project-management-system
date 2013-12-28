b502cc820ee73b09b41c6d3b3d9d47c689e00296
Alexey Krylov <alexey2142@gmail.com>
2013-12-28 11:09:27 +1100

    Сделал вывод затраченного времени по задачам в статистике проекта

--- app/views/statistics/projects.blade.php
+++ app/views/statistics/projects.blade.php
@@ -15,17 +15,28 @@
 		</thead>
 
 		<tbody>
+			<?php $all_time = 0; ?>
 			@foreach($project->tasks as $task)
 				<tr>
 					<td>{{{ $task->name }}}</td>
 					<td>{{{ $task->employee ? $task->employee->name : 'Нет' }}}</td>
-					<td>{{{ $task->work_time }}}</td>
+					<?php
+						$diff = 0;
+						foreach($task->reports as $report)
+						{
+							$start = date_create($report->time_start);
+							$end = date_create($report->time_end);
+							$diff += $end->diff($start)->format("%h");
+						}
+						$all_time += $diff;
+					?>
+					<td>{{{ $diff }}} часов</td>
 				</tr>
 			@endforeach
 		</tbody>
 	</table>
 
-	Всего затрачено времени: {{{ array_sum($task->lists('work_time')) }}}
+	Всего затрачено времени: {{{ $all_time }}} часов
 @else
 	Нет деятельности
 @endif
